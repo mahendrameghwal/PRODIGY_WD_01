04c84797c866fe751facd770ccfacd10dfdf2ca1
mahendra <solankirakesh895@gmail.com>
2024-07-31 14:40:38 +0530

    update:.env prod

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-
-
-export default defineConfig({
-  plugins: [ react() ],
-  server: {
-    port: 5173,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: env.APP_ENV
+    },
   }
 })
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+
+
+
+// export default defineConfig({
+//   plugins: [ react() ],
+//   server: {
+//     port: 5173,
+//   }
+// })
